41ad6c2a8b89c081cc6119e75f261124d2ffc3a9
Julien <julien@nozav.org>
2019-12-09 18:29:55 +0100

    Add and buildignore travai.R

---
+++ travail.R
@@ -0,0 +1,12 @@
+devtools::load_all(".")
+
+iris_num <- iris %>% select_if(is.numeric)
+iris_sup <- iris %>% select(Species)
+us <- new_umapscan(iris_num, data_sup = iris_sup, n_neighbors = 25, min_dist = 0.1, seed = 1111)
+us <- compute_clusters(us, minPts = 20, eps = 1, alpha = 1)
+us2 <- compute_clusters(us, minPts = 3, eps = 0.5, alpha = 1, parent = "3")
+us3 <- compute_clusters(us2, minPts = 3, eps = 0.2, alpha = 1, parent = "3_1")
+
+rename_cluster(us3, "3_1_1", "foo")
+
+us3
