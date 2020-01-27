449a65d9c81eb760805cfda2d6e7464f8e0547c6
Julien <julien@nozav.org>
2020-01-27 11:57:54 +0100

    Fix tests

--- tests/testthat/test_umapscan.R
+++ tests/testthat/test_umapscan.R
@@ -13,8 +13,7 @@ test_that("new_umapscan results are ok", {
   us <- new_umapscan(iris_num, data_sup = iris_sup, seed = 1337, scale = FALSE)
   expect_equal(us$data, iris_num)
   expect_equal(us$data_sup, iris_sup)
-  expect_equal(us$cluster, rep(NA_character_, nrow(iris_num)))
-  expect_equal(us$cluster_validated, rep(FALSE, nrow(iris_num)))
+  expect_equal(dim(us$cluster), c(0, 5))
   expect_equal(dim(us$umap), c(nrow(iris_num), 2))
   set.seed(1337)
   umap <- uwot::umap(iris_num)
@@ -24,7 +23,7 @@ test_that("new_umapscan results are ok", {
 test_that("new_umapscan results with scale=TRUE are ok", {
   us <- new_umapscan(iris_num, data_sup = iris_sup, seed = 24312, scale = TRUE)
   set.seed(24312)
-  umap <- uwot::umap(mutate_all(iris_num, base::scale))
+  umap <- uwot::umap(dplyr::mutate_all(iris_num, base::scale))
   expect_equal(tibble::tibble(x = umap[,1], y = umap[,2]), us$umap)
 })
 
