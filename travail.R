78f86a61bf98cd6ba386316a13fa88b4cb71bc04
Julien <julien@nozav.org>
2020-01-27 16:18:21 +0100

    Update

--- travail.R
+++ travail.R
@@ -6,6 +6,8 @@ iris_sup <- iris %>% select(Species)
 us <- new_umapscan(iris_num, data_sup = iris_sup, n_neighbors = 25, min_dist = 0.1, seed = 1111)
 us
 us <- compute_clusters(us, minPts = 20, eps = 1, alpha = 1, graph = TRUE)
+describe_clusters(us)
+
 us2 <- compute_clusters(us, minPts = 3, eps = 0.5, alpha = 1, parent = "3")
 us3 <- compute_clusters(us2, minPts = 3, eps = 0.2, alpha = 1, parent = "3_2")
 us3
