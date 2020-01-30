1e2d137975f5fdca30f71590408fdcfd76a72ecd
Julien <julien@nozav.org>
2020-01-30 15:15:43 +0100

    Move expected results to files and add tests for get_leaves and get_ids

--- tests/testthat/test_clustering.R
+++ tests/testthat/test_clustering.R
@@ -8,73 +8,74 @@ us <- compute_clusters(us, minPts = 3, parent = "3", eps = 0.4, graph = FALSE)
 us <- compute_clusters(us, minPts = 3, parent = "2", eps = 0.15, graph = FALSE)
 
 test_that("get cluster memberships", {
-  res <- c("1", "2_1", "2_2", "2_4", "1", "1", "1", "1", "2_3", "2_4",
-    "1", "1", "2_4", "2_3", "1", "1", "1", "1", "1", "1", "1", "1",
-    "1", "1", "1", "2_1", "1", "1", "1", "2_2", "2_4", "1", "1",
-    "1", "2_4", "<Noise>", "1", "1", "2_3", "1", "1", "2_3", "2_2",
-    "1", "1", "2_1", "1", "2_2", "1", "1", "3_1", "3_1", "3_1", "3_2",
-    "3_1", "3_3", "3_1", "3_2", "3_1", "3_2", "3_2", "3_4", "3_2",
-    "3_4", "3_3", "3_1", "3_3", "3_2", "3_2", "3_2", "3_5", "3_4",
-    "3_5", "3_4", "3_1", "3_1", "3_1", "<Noise>", "3_4", "3_2", "3_2",
-    "3_2", "3_2", "3_5", "3_3", "3_1", "3_1", "3_2", "3_3", "3_2",
-    "3_2", "3_4", "3_2", "3_2", "3_2", "3_3", "3_3", "3_4", "3_2",
-    "3_3", "3_6", "3_5", "3_6", "3_6", "3_6", "3_6", "3_2", "3_6",
-    "3_5", "3_6", "3_6", "3_5", "3_6", "3_5", "3_5", "3_6", "3_6",
-    "3_6", "3_6", "3_2", "3_6", "3_5", "3_6", "3_5", "3_6", "3_6",
-    "3_5", "3_5", "3_6", "3_6", "3_6", "3_6", "3_6", "3_5", "3_5",
-    "3_6", "3_6", "3_6", "3_5", "3_6", "3_6", "3_6", "3_5", "3_6",
-    "3_6", "3_6", "3_5", "3_6", "3_6", "3_5")
-  expect_equal(get_clusters_membership(us), res)
-  res <- c(NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA,
-    NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA,
-    NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA,
-    NA, NA, NA, "3_1", "3_1", "3_1", "3_2", "3_1", "3_3", "3_1",
-    "3_2", "3_1", "3_2", "3_2", "3_4", "3_2", "3_4", "3_3", "3_1",
-    "3_3", "3_2", "3_2", "3_2", "3_5", "3_4", "3_5", "3_4", "3_1",
-    "3_1", "3_1", "<Noise>", "3_4", "3_2", "3_2", "3_2", "3_2", "3_5",
-    "3_3", "3_1", "3_1", "3_2", "3_3", "3_2", "3_2", "3_4", "3_2",
-    "3_2", "3_2", "3_3", "3_3", "3_4", "3_2", "3_3", "3_6", "3_5",
-    "3_6", "3_6", "3_6", "3_6", "3_2", "3_6", "3_5", "3_6", "3_6",
-    "3_5", "3_6", "3_5", "3_5", "3_6", "3_6", "3_6", "3_6", "3_2",
-    "3_6", "3_5", "3_6", "3_5", "3_6", "3_6", "3_5", "3_5", "3_6",
-    "3_6", "3_6", "3_6", "3_6", "3_5", "3_5", "3_6", "3_6", "3_6",
-    "3_5", "3_6", "3_6", "3_6", "3_5", "3_6", "3_6", "3_6", "3_5",
-    "3_6", "3_6", "3_5")
-  expect_equal(get_clusters_membership(us, parent = "3"), res)
-  res <- c("1", "2", "2", "2", "1", "1", "1", "1", "2", "2", "1", "1",
-    "2", "2", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1",
-    "2", "1", "1", "1", "2", "2", "1", "1", "1", "2", "2", "1", "1",
-    "2", "1", "1", "2", "2", "1", "1", "2", "1", "2", "1", "1", "3",
-    "3", "3", "3", "3", "3", "3", "3", "3", "3", "3", "3", "3", "3",
-    "3", "3", "3", "3", "3", "3", "3", "3", "3", "3", "3", "3", "3",
-    "3", "3", "3", "3", "3", "3", "3", "3", "3", "3", "3", "3", "3",
-    "3", "3", "3", "3", "3", "3", "3", "3", "3", "3", "3", "3", "3",
-    "3", "3", "3", "3", "3", "3", "3", "3", "3", "3", "3", "3", "3",
-    "3", "3", "3", "3", "3", "3", "3", "3", "3", "3", "3", "3", "3",
-    "3", "3", "3", "3", "3", "3", "3", "3", "3", "3", "3", "3", "3",
-    "3", "3", "3", "3", "3", "3", "3", "3")
-  expect_equal(get_clusters_membership(us, max_level = 1), res)
-  res <- c(NA, "2_1", "2_2", "2_4", NA, NA, NA, NA, "2_3", "2_4", NA,
-    NA, "2_4", "2_3", NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA,
-    "2_1", NA, NA, NA, "2_2", "2_4", NA, NA, NA, "2_4", "2", NA,
-    NA, "2_3", NA, NA, "2_3", "2_2", NA, NA, "2_1", NA, "2_2", NA,
-    NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA,
-    NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA,
-    NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA,
-    NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA,
-    NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA,
-    NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA,
-    NA, NA, NA, NA, NA)
-  expect_equal(get_clusters_membership(us, noise_inherit_parent = TRUE, parent = "2"), res)
-  expect_error(get_clusters_membership(us, parent = "<Noise>"), "Can't get membership starting from a <Noise> node")
-  expect_equal(get_clusters_membership(us, noise_inherit_parent = TRUE, parent = "8"), rep(NA_character_, 150))
+  # saveRDS(get_clusters_membership(us), "tests/values/get_cluster_membership1.rds")
+  expect_equal(
+    get_clusters_membership(us),
+    readRDS("../values/get_cluster_membership1.rds")
+  )
+  # saveRDS(get_clusters_membership(us, parent = "3"), "tests/values/get_cluster_membership2.rds")
+  expect_equal(
+    get_clusters_membership(us, parent = "3"),
+    readRDS("../values/get_cluster_membership2.rds")
+  )
+  # saveRDS(get_clusters_membership(us, max_level = 1), "tests/values/get_cluster_membership3.rds")
+  expect_equal(
+    get_clusters_membership(us, max_level = 1),
+    readRDS("../values/get_cluster_membership3.rds")
+  )
+  # saveRDS(get_clusters_membership(us, noise_inherit_parent = TRUE, parent = "2"), "tests/values/get_cluster_membership4.rds")
+  expect_equal(
+    get_clusters_membership(us, noise_inherit_parent = TRUE, parent = "2"),
+    readRDS("../values/get_cluster_membership4.rds")
+  )
+  expect_error(
+    get_clusters_membership(us, parent = "<Noise>"),
+    "Can't get membership starting from a <Noise> node"
+  )
+  expect_equal(
+    get_clusters_membership(us, noise_inherit_parent = TRUE, parent = "8"),
+    rep(NA_character_, 150)
+  )
 })
 
 
-# test_that("get cluster leaves", {
-#   get_leaves(us)
-#   get_leaves(us, node = "2")
-# })
+test_that("get_leaves", {
+  res <- structure(list(from = c("", "2", "2", "2", "2", "2", "3", "3",
+    "3", "3", "3", "3", "3"), to = c("1", "2_1", "2_2", "2_4", "2_3",
+      "<Noise>", "3_1", "3_2", "3_3", "3_4", "3_5", "<Noise>", "3_6"
+    )), row.names = c(NA, -13L), class = c("tbl_df", "tbl", "data.frame"
+    ))
+  expect_equal(umapscan:::get_leaves(us$clusters), res)
+  res <- structure(list(from = c("2", "2", "2", "2", "2"), to = c("2_1",
+    "2_2", "2_4", "2_3", "<Noise>")), row.names = c(NA, -5L), class = c("tbl_df",
+      "tbl", "data.frame"))
+  expect_equal(umapscan:::get_leaves(us$clusters, node = "2"), res)
+  res <- structure(list(from = "foo", to = "1"), row.names = c(NA, -1L
+  ), class = c("tbl_df", "tbl", "data.frame"))
+  expect_equal(umapscan:::get_leaves(us$clusters, node = "1", parent = "foo"), res)
+  expect_error(
+    umapscan:::get_leaves(us$clusters, node = "<Noise>"),
+    "Can't get leaves from a <Noise> node."
+  )
+})
+
+test_that("get_ids", {
+  expect_error(umapscan:::get_ids(us))
+  expect_equal(
+    get_ids(us, "2"),
+    c(2L, 3L, 4L, 9L, 10L, 13L, 14L, 26L, 30L, 31L, 35L, 36L, 39L,
+      42L, 43L, 46L, 48L)
+  )
+  expect_equal(
+    get_ids(us, "3_4"),
+    c(62L, 64L, 72L, 74L, 79L, 92L, 98L)
+  )
+  expect_null(get_ids(us, "3_8"))
+  expect_equal(get_ids(us, ""), 1:nrow(iris))
+  expect_error(get_ids(us, "<Noise>"), "Can't get ids from a <Noise> node.")
+})
+
+
 
 
 
