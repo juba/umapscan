d3c3f800c42e72c1d37c684ec8688ffac2e27a7f
Julien <julien@nozav.org>
2020-03-20 18:30:16 +0100

    Add categorical and textual examples

--- travail.R
+++ travail.R
@@ -5,13 +5,38 @@ iris_num <- iris %>% select_if(is.numeric)
 iris_sup <- iris %>% select(Species)
 us <- new_umapscan(iris_num, data_sup = iris_sup, n_neighbors = 25, min_dist = 0.1, seed = 1111)
 us
-us <- compute_clusters(us, minPts = 20, eps = 1, alpha = 1, graph = TRUE)
+us <- compute_clusters(us, minPts = 3, eps = 1, alpha = 1)
 describe_clusters(us)
 
-us2 <- compute_clusters(us, minPts = 3, eps = 0.5, alpha = 1, parent = "3")
-us3 <- compute_clusters(us2, minPts = 3, eps = 0.2, alpha = 1, parent = "3_2")
+us2 <- compute_clusters(us, minPts = 3, eps = 0.6, alpha = 1, parent = "2")
+us3 <- compute_clusters(us2, minPts = 3, eps = 0.3, alpha = 1, parent = "2_2")
 us3
 
-rename_cluster(us3, "3_1_1", "foo")
+us4 <- compute_clusters(us3, minPts = 3, eps = 0.3, alpha = 1, parent = "1")
+us4
 
-us3
+## categorical
+
+gen_var <- function() { sample(c(0,1), 100, replace = TRUE) }
+set.seed(13)
+df <- data.frame(v1 = gen_var(), v2 = gen_var(), v3 = gen_var())
+us <- new_umapscan(df, n_neighbors = 25, min_dist = 0.1, seed = 1111, metric = "cosine")
+us <- compute_clusters(us, minPts = 3, eps = 0.4)
+describe_clusters(us, type = "barplot")
+
+## textual
+
+library(quanteda)
+corpus <- data_corpus_inaugural
+dtm <- dfm(corpus, remove_punct = TRUE)
+dtm <- dfm_trim(dtm, min_termfreq = 50)
+
+us <- new_umapscan(convert(dtm, to="data.frame"), n_neighbors = 5, min_dist = 0.1, metric = "cosine")
+us <- compute_clusters(us, minPts = 3, eps = 1)
+describe_clusters(us, type = "keyness")
+us2 <- compute_clusters(us, minPts = 3, eps = .5, parent = "2")
+describe_clusters(us2, type = "keyness")
+describe_clusters(us2, type = "keyness", n_terms = 10, free_scale = FALSE, text_size = 15)
+describe_clusters(us2, type = "keyness", keyness_measure = "lr")
+describe_clusters(us2, type = "keyness", keyness_measure = "exact")
+describe_clusters(us2, type = "keyness", keyness_measure = "pmi")
