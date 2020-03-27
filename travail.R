d46392a1808c2d0fb720efb87064663418afdf77
Julien <julien@nozav.org>
2020-03-27 16:27:26 +0100

    Renaming

--- travail.R
+++ travail.R
@@ -2,21 +2,21 @@
 iris_num <- iris[, c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width")]
 iris_sup <- iris[, "Species", drop = FALSE]
 us <- new_umapscan(iris_num, data_sup = iris_sup, seed = 1337, scale = TRUE)
-us <- compute_clusters(us, minPts = 3, eps = 1, graph = FALSE)
-describe_clusters(us)
+us <- clust_compute(us, minPts = 3, eps = 1, graph = FALSE)
+clust_describe(us)
 
-us2 <- compute_clusters(us, minPts = 3, eps = 0.3, alpha = 1, parent = "2")
-us3 <- compute_clusters(us2, minPts = 3, eps = 0.2, alpha = 1, parent = "2_1")
+us2 <- clust_compute(us, minPts = 3, eps = 0.3, alpha = 1, parent = "2")
+us3 <- clust_compute(us2, minPts = 3, eps = 0.2, alpha = 1, parent = "2_1")
 us3
 
-us4 <- compute_clusters(us3, minPts = 3, eps = 0.3, alpha = 1, parent = "1")
+us4 <- clust_compute(us3, minPts = 3, eps = 0.3, alpha = 1, parent = "1")
 us4
 
 
 us4$clusters
-us4 <- label_cluster(us4, "3", "Foobar")
-get_clusters_membership(us4)
-get_clusters_membership(us4, labels = FALSE)
+us4 <- clust_label(us4, "3", "Foobar")
+clust_members(us4)
+clust_members(us4, labels = FALSE)
 
 ## categorical
 
@@ -24,8 +24,8 @@ gen_var <- function() { sample(c(0,1), 100, replace = TRUE) }
 set.seed(13)
 df <- data.frame(v1 = gen_var(), v2 = gen_var(), v3 = gen_var())
 us <- new_umapscan(df, n_neighbors = 25, min_dist = 0.1, seed = 1111, metric = "cosine")
-us <- compute_clusters(us, minPts = 3, eps = 0.4)
-describe_clusters(us, type = "barplot")
+us <- clust_compute(us, minPts = 3, eps = 0.4)
+clust_describe(us, type = "barplot")
 
 ## textual
 
@@ -35,11 +35,11 @@ dtm <- dfm(corpus, remove_punct = TRUE)
 dtm <- dfm_trim(dtm, min_termfreq = 50)
 
 us <- new_umapscan(dtm, n_neighbors = 5, min_dist = 0.1, metric = "cosine")
-us <- compute_clusters(us, minPts = 3, eps = 1)
-describe_clusters(us, type = "keyness")
-us2 <- compute_clusters(us, minPts = 3, eps = .3, parent = "2")
-describe_clusters(us2, type = "keyness")
-describe_clusters(us2, type = "keyness", n_terms = 10, free_scale = FALSE, text_size = 15)
-describe_clusters(us2, type = "keyness", keyness_measure = "lr")
-describe_clusters(us2, type = "keyness", keyness_measure = "exact")
-describe_clusters(us2, type = "keyness", keyness_measure = "pmi")
+us <- clust_compute(us, minPts = 3, eps = 1)
+clust_describe(us, type = "keyness")
+us2 <- clust_compute(us, minPts = 3, eps = .3, parent = "2")
+clust_describe(us2, type = "keyness")
+clust_describe(us2, type = "keyness", n_terms = 10, free_scale = FALSE, text_size = 15)
+clust_describe(us2, type = "keyness", keyness_measure = "lr")
+clust_describe(us2, type = "keyness", keyness_measure = "exact")
+clust_describe(us2, type = "keyness", keyness_measure = "pmi")
